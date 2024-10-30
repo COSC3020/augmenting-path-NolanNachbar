7212a2d1ead77eb8b2fbf20fdbea8de7295756f7
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-10-30 15:30:40 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,25 @@
-function augmentingPath(graph, start, end) {
-    return [];
+function augmentingPath(graph, start, end) { // note that graph will be an adjacency list
+    let visited = [] 
+    let path = []
+    
+    function depthFirstHelp(node){
+        if (visited.includes(node)) return false;
+        if (node === end) {
+          path.push(node);
+          return true;
+        }
+        
+        visited.push(node);
+        path.push(node);
+        for(let i = 0; i < graph[node].length; i++){
+            if(depthFirstHelp(graph[node][i]))
+              return true;
+        }
+        
+        path.pop(); // pop it because the target wasn't found on it's path
+        return false;
+    }
+    
+    depthFirstHelp(start);
+    return path;
 }
-
